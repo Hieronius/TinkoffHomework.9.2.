1a82fa6e427ae5b584da0e289b70eb823d841560
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-21 14:39:52 +0300

    Add action method to the buttons

--- TinkoffHomework.9.2./TinkoffHomework.9.2./ViewController.swift
+++ TinkoffHomework.9.2./TinkoffHomework.9.2./ViewController.swift
@@ -2,12 +2,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - App life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    // MARK: - Action methods
+    
+    @IBAction func pressButton(_ sender: UIButton) {
+        print(sender.titleLabel!.text!);
+    }
+    
 
 }
 
