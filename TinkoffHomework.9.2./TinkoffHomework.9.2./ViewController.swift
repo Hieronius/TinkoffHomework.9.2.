420949addeb0ab8a1959dd0fb1e31d16ef5abc83
Arsenty Halimovsky <ghost12@yandex.ru>
2024-01-21 06:11:36 +0300

    Create a new project

---
+++ TinkoffHomework.9.2./TinkoffHomework.9.2./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TinkoffHomework.9.2.
+//
+//  Created by Арсентий Халимовский on 21.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
